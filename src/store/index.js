35c58212403cb4e310cce5ce80d32ef82cc4bcdc
hhf <1170533231@qq.com>
2019-06-10 21:44:46 +0800

    更新

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ import createSagaMiddleware from 'redux-saga'
 import reducer from './reducers'
 import rootSaga from './sagas'
 export default function configureStore(initialState) {
-  // 注意：必须满足 redux@>=3.1.0 才可以将 middleware 作为 createStore 的最后一个参数传递
+  // 注意：必须满足 redux@>=3.1.0 才可r 以将 middleware 作为 createStore 的最后一个参数传递
 
   const sagaMiddleware = createSagaMiddleware()
       const store = createStore(
